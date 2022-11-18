c3296385d82019763be0a1394852c5bf670d04ff
Dennis Sanders <dennis.sanders@cdiproducts.com>
2022-11-18 13:22:02 -0600

    Add css and scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 let mode = "development";
 
 if (process.env.NODE_ENV === "production") {
@@ -10,6 +12,15 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /.s?css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader"
+        ],
+      },
+      {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
@@ -19,8 +30,11 @@ module.exports = {
     ],
   },
 
+  plugins: [new MiniCssExtractPlugin()],
+
   devtool: "source-map",
   devServer: {
     static: "./dist",
+    hot: true,
   },
 }
